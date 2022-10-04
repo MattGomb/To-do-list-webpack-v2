99b49fa02e6370af4e5a94e38de8c359875a6a1e
Matyas Gombos <gombosmm@gmail.com>
2022-10-04 10:57:07 -0500

    Set up live server through webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static:'./dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
